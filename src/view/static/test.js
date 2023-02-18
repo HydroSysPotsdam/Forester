b7c3451a00f522f6a41d6b7c9269d62fcdadfeec
David Strahl <101013522+david-strahl@users.noreply.github.com>
2023-02-18 09:18:49 +0100

    updated requirements.txt

--- src/view/static/test.js
+++ src/view/static/test.js
@@ -4,8 +4,6 @@
  * Forester: Interactive human-in-the-loop web-based visualization of machine learning trees
  */
 
-import {Tree} from "./editor/TreeRenderer.js";
-
 Tree.fetch_json("../../../examples/R/diabetes.json")
     .then(draw_box1)
 

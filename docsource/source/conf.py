10b7fbc511fa325f532cbf83356151d9c0a495b0
David Strahl <101013522+david-strahl@users.noreply.github.com>
2023-03-12 10:14:38 +0100

    Updated social media image.

--- docsource/source/conf.py
+++ docsource/source/conf.py
@@ -94,6 +94,11 @@ html_sidebars = {
     ]
 }
 
+ogp_social_cards = False
+ogp_site_url = "https://hydrosyspotsdam.github.io/Forester/"
+ogp_image = "./_static/logo.svg"
+ogp_use_first_image = False
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".

0501d02dc7abe78274de86a1ea3fd3d5d8dd89d7
David Strahl <101013522+david-strahl@users.noreply.github.com>
2023-03-14 08:55:57 +0100

    Fixed regeneration of database issue.

--- src/run.py
+++ src/run.py
@@ -4,7 +4,14 @@
 
 from flask import Flask
 from forester import server
+import webbrowser
+import os
 
 if __name__ == "__main__":
     app = server.create_app()
+
+    # The reloader has not yet run - open the browser
+    if not os.environ.get("WERKZEUG_RUN_MAIN"):
+        webbrowser.open_new('http://127.0.0.1:8000/')
+
     Flask.run(app, port=8000, debug=False)
